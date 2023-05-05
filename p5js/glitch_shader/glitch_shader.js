e4370465c2adec623b23f77057142f5af13a5402
skshm <eminate894567@protonmail.com>
2023-05-05 11:11:27 +0530

    Glitch shader

--- p5js/glitch_shader/glitch_shader.js
+++ p5js/glitch_shader/glitch_shader.js
@@ -1,21 +1,52 @@
 let screen;
 
+let glitchShader;
+
+function preload() {
+    glitchShader = loadShader('shader.vert', 'shader.frag');
+}
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight, WEBGL);
     screen = createGraphics(width, height);
 
-    screen.background(50);
+    screen.background(20);
     screen.stroke(255);
     screen.strokeWeight(4);
+
+    shader(glitchShader);
 
 }
 
 
 function draw() {
-    image(screen, 0,0);
+    image(screen, -width/2, -height/2, width, height);
 
     if(mouseIsPressed){
         screen.line(mouseX, mouseY, pmouseX, pmouseY);
     }
 
+    drawScreen();
+
 }
+
+function drawScreen(){
+    glitchShader.setUniform('texture', screen);
+    glitchShader.setUniform('noise', getNoiseValue());
+
+    rect(-width/2, -height/2, width, height);
+}
+function getNoiseValue() {
+    let v = noise(millis()/100);
+    const cutoff = 0.5;
+    if (v<cutoff) {
+        return 0;
+    }
+    v = pow((v-cutoff)/(1-cutoff), 2);
+
+    return v;
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}  
