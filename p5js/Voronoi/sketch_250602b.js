5e5ff0ddc3dcc66048c3368c4b25b627d29b2dc9
skshm <eminate894567@protonmail.com>
2025-06-02 19:38:25 +0530

    images and script

--- p5js/Voronoi/sketch_250602b.js
+++ p5js/Voronoi/sketch_250602b.js
@@ -3,12 +3,25 @@ let delauany, voronoi;
 let imaged;
 
 function preload() {
-  imaged = loadImage("imgs/Mountain Fog.JPG");
+  imaged = loadImage("imgs/girlbimbo-insta-revBG.png");
 }
 
 function setup() {
-  createCanvas(300*2, 300*3);
-  for (let i = 0; i < 3000; i++) {
+  let w = imaged.width;
+  let h = imaged.height;
+  let maxW = windowWidth;
+  let maxH = windowHeight;
+  let scale = min(maxW / w, maxH / h, 1);
+  createCanvas(w * scale, h * scale);
+  imaged.resize(w * scale, h * scale);
+
+  makedots(20_000);
+  delauany = calculateDelaunay(points);
+  voronoi = delauany.voronoi([0, 0, width, height]);
+}
+
+function makedots(dots) {
+  for (let i = 0; i < dots; i++) {
     let x = random(width);
     let y = random(height);
 
@@ -19,74 +32,62 @@ function setup() {
       i--;
     }
   }
-  delauany = calculateDelaunay(points);
-  voronoi = delauany.voronoi([0, 0, width, height]);
-
-//   noLoop();
 }
 
 function draw() {
   background(0);
 
-  for (let v of points) {
-    stroke(255);
-    strokeWeight(4);
-    point(v.x, v.y);
-    fill(0, 100);
-  }
+  for (let v of points) {    //! make points
 
-  //   noFill();
-  //   strokeWeight(1);
-  //   let { dPoints, triangles } = delauany;
-  //   for (let i = 0; i < triangles.length; i += 3) {
-  //     let a = 2 * delauany.triangles[i];
-  //     let b = 2 * delauany.triangles[i + 1];
-  //     let c = 2 * delauany.triangles[i + 2];
-  //     triangle(
-  //       dPoints[a],
-  //       dPoints[a + 1],
-  //       dPoints[b],
-  //       dPoints[b + 1],
-  //       dPoints[c],
-  //       dPoints[c + 1]
-  //     );
-  //   }
+    let col = imaged.get(v.x, v.y); // Get color from image at point's position
+    let bright = brightness(col);  // 0 (dark) to 100 (bright)
+    let size = map(bright, 100, 0, 8, 2);
+    stroke(col);
+    //stroke(255)
+    //Map brightness to size: darker = bigger, e.g. 2 (bright) to 8 (dark)
+    strokeWeight(size);
+    //strokeWeight(3);
+    point(v.x, v.y);
+  }
 
   let polygons = voronoi.cellPolygons();
   let cells = Array.from(polygons);
 
-  //   //? Making the polygons
-  //   for (let poly of cells) {
-  //     stroke(255);
-  //     strokeWeight(2);
-  //     noFill();
-  //     beginShape();
-  //     for (let i = 0; i < poly.length; i++) {
-  //       vertex(poly[i][0], poly[i][1]);
-  //     }
-  //     endShape();
-  //   }
+  let centroidArr = new Array(cells.length);
+  let weightArr = new Array(cells.length).fill(0);
+  for (let i = 0; i < cells.length; i++) {
+    centroidArr[i] = createVector(0, 0);
+  }
 
-  //? Centroids, center of each poly
-  let arrCentroid = [];
-  for (let poly of cells) {
-    let area = 0;
-    let centroid = createVector(0, 0);
-    for (let i = 0; i < poly.length; i++) {
-      let v0 = poly[i];
-      let v1 = poly[(i + 1) % poly.length]; // cross product for last vector
-      let crossProd = v0[0] * v1[1] - v1[0] * v0[1];
-      area += crossProd;
-      centroid.x += (v0[0] + v1[0]) * crossProd;
-      centroid.y += (v0[1] + v1[1]) * crossProd;
+  imaged.loadPixels();
+  let delauanyIdx = 0;
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      let index = (i + j * width) * 4; // 4 values, RGBA
+      let r = imaged.pixels[index + 0];
+      let g = imaged.pixels[index + 1];
+      let b = imaged.pixels[index + 2];
+      let bright = 0.2126 * r + 0.7152 * g + 0.0722 * b; //relative luminance
+      let weight = 1 - bright / 255;
+
+      delauanyIdx = delauany.find(i, j, delauanyIdx); // lookup which centroid to pair to
+      centroidArr[delauanyIdx].x += i * weight; // add
+      centroidArr[delauanyIdx].y += j * weight; // vals
+      weightArr[delauanyIdx] += weight; // add weight to sum
     }
-    area /= 2;
-    centroid.div(6 * area);
-    arrCentroid.push(centroid);
+  }
+
+  // look at centroid, divide by sum of weights
+  for (let i = 0; i < centroidArr.length; i++) {
+    if (weightArr[i] > 0) {
+      centroidArr[i].div(weightArr[i]);
+    } else {
+      centroidArr[i] = points[i].copy(); // if no weight, use original point
+    }
   }
 
   for (let i = 0; i < points.length; i++) {
-    points[i].lerp(arrCentroid[i], 1);
+    points[i].lerp(centroidArr[i], 0.2);
   }
 
   delauany = calculateDelaunay(points);
